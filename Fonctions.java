75174e8ddeb1947684236e0fe1bbb49b7d695ee9
Dalya <dalyfuraiji@laposte.net>
2019-12-01 14:57:36 +0100

    Serveur.java: ajout SSL pour Serveur et Fonctions

--- Fonctions.java
+++ Fonctions.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.*;
-
-import javax.crypto.NullCipher;
-
+import javax.net.ssl.*;
 public class Fonctions implements Runnable{ 
     
-    private Socket socket;
+    private SSLSocket socket;
     private PrintWriter out ;
     private BufferedReader in ;
     private String protocole;
@@ -15,16 +13,17 @@ public class Fonctions implements Runnable{
     private static ArrayList<Annonce> annoncesAll = new ArrayList<Annonce>();
 
     
-    public Fonctions(Socket _socket) {
-        this.socket = _socket;
+    public Fonctions(SSLSocket _socket) {
+        this.socket = (SSLSocket) _socket;
     }
 
     @Override
     public void run() {
     	User u = null;
         try {
+            
             out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));             
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));  
             String str = "";
             String [] token = null;
             while ((str = in.readLine()) != null) {
@@ -35,7 +34,7 @@ public class Fonctions implements Runnable{
                 }else{
                     token = str.split(";");
                     protocole = token[0];
-                }   
+                } 
                 switch (protocole) {
                     case "CONNECT":
                         u = connect(token);
--- Serveur.java
+++ Serveur.java
@@ -1,20 +1,30 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.*;
+import java.security.Security;
 
 public class Serveur{
 
+    
     public static void main(String[] args){
         
+		System.setProperty("javax.net.ssl.keyStore", "server.jsk");
+		System.setProperty("javax.net.ssl.keyStorePassword" , "123456");
+		SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+		
         try{
-                    
-            ServerSocket server = new ServerSocket(1027);
-            System.out.println("..... Bienvenue sur notre Serveur ....");
+            
+			SSLServerSocket server = (SSLServerSocket)factory.createServerSocket(1027);
+            System.out.println("..... Bienvenue sur notre Serveur ....");   
             while(true){
             	System.out.println(server.getInetAddress().getLocalHost().getHostAddress());
-                Socket socket = server.accept();
+                SSLSocket socket = (SSLSocket)server.accept();
                 Fonctions serv = new Fonctions(socket);
                 Thread t = new Thread(serv);
                 t.start();  
+                
             }
         }catch(Exception e){
             System.out.println("Serveur ne peut pas connecter");
